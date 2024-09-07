1f4e2d37644e012de148a5e66c7c698d52cd00fa
PAPAT16 <timilehinsamuel505@gmail.com>
2024-09-07 20:55:48 +0100

    applecommit

---
+++ src/views/Storeview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Store">
+    <div class="ms-4">
+      <h1 class="theh"><span class="ore">Store.</span>The best way to buy the <br />products you love.</h1>
+      <div class="d-inline ms-5">
+        <span class="d-inline-flex">
+          <img
+            src="@/assets/assets (1)/store-chat-specialist-icon-202309_AV2.jpeg"
+            alt=""
+            width="40px"
+          />
+          <p>Need a shopping help?</p>
+        </span>
+        <p><a href="#" class="text-decoration-none">Ask a specialist</a></p>
+      </div>
+
+      <div class="d-inline">
+        <span class="d-inline-flex">
+          <img src="@/assets/assets (1)/asset 119.svg" alt="" width="40px" />
+          <p>Visit An Apple Store</p>
+        </span>
+        <p><a href="#" class="text-decoration-none">Find one near you ></a></p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.Store {
+  background-color: rgb(245, 245, 247);
+  font-family: sans-serif;
+}
+.theh {
+  color: rgb(110, 110, 115);
+  font-weight: 600;
+  text-align: left;
+  padding-top: 90px;
+}
+.ore {
+  color: black;
+}
+</style>
